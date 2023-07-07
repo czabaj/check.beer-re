f6454fca0b820a1c6ba1af64a0679845f695cc12
Václav Gröhling <grohling.v@clav.cz>
2023-07-07 07:58:38 +0200

    feat: style for depleted kegs

--- src/pages/PlaceSetting/DepletedKegs.stories.js
+++ src/pages/PlaceSetting/DepletedKegs.stories.js
@@ -1,3 +1,4 @@
+import * as Belt_MapString from "rescript/lib/es6/belt_MapString.js";
 import { Timestamp } from "firebase/firestore";
 
 import { make } from "./DepletedKegs.bs.js";
@@ -6,6 +7,85 @@ const getKegMock = (consumptionLevel) => {
   let volume = 30000;
   return {
     beer: "Pilsner Urquell",
+    consumptions: Belt_MapString.fromArray([
+      [
+        "1688362302044",
+        {
+          milliliters: 500,
+          person: {},
+        },
+      ],
+      [
+        "1688364131453",
+        {
+          milliliters: 300,
+          person: {},
+        },
+      ],
+      [
+        "1688364133571",
+        {
+          milliliters: 500,
+          person: {},
+        },
+      ],
+      [
+        "1688364226633",
+        {
+          milliliters: 300,
+          person: {},
+        },
+      ],
+      [
+        "1688405703848",
+        {
+          person: {},
+          milliliters: 500,
+        },
+      ],
+      [
+        "1688405705838",
+        {
+          milliliters: 500,
+          person: {},
+        },
+      ],
+      [
+        "1688405707136",
+        {
+          milliliters: 500,
+          person: {},
+        },
+      ],
+      [
+        "1688405708358",
+        {
+          milliliters: 500,
+          person: {},
+        },
+      ],
+      [
+        "1688405714687",
+        {
+          milliliters: 500,
+          person: {},
+        },
+      ],
+      [
+        "1688405716223",
+        {
+          milliliters: 500,
+          person: {},
+        },
+      ],
+      [
+        "1688405717366",
+        {
+          milliliters: 500,
+          person: {},
+        },
+      ],
+    ]),
     consumptionsSum: consumptionLevel * volume,
     createdAt: Timestamp.fromMillis(1688481980 * 1000),
     depletedAt: Timestamp.now(),
